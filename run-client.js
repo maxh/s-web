cc2cefb93c7a85bca3faa224714e80945cded0e6
Max Heinritz <maxheinritz@gmail.com>
2017-01-24 23:17:21 -0800

    prep for heroku

--- run-client.js
+++ run-client.js
@@ -1,5 +1,7 @@
 var yargs = require('yargs').argv;
 
 const args = [ yargs.command ];
-const opts = { stdio: 'inherit', cwd: 'client', shell: true };
+var env = Object.create(process.env);
+env.NODE_ENV = yargs.env;
+const opts = { stdio: 'inherit', cwd: 'client', shell: true, env: env };
 require('child_process').spawn('npm', args, opts);
