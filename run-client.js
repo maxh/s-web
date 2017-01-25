2b0c32c3bf89b771f2d9dd309d1c20ed697d101e
Max Heinritz <maxheinritz@gmail.com>
2017-01-24 16:48:15 -0800

    deploy

--- run-client.js
+++ run-client.js
@@ -1,3 +1,5 @@
-const args = [ 'start' ];
+var yargs = require('yargs').argv;
+
+const args = [ yargs.command ];
 const opts = { stdio: 'inherit', cwd: 'client', shell: true };
 require('child_process').spawn('npm', args, opts);
