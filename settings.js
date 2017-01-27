6c02ca10790743b4de15babd95b96519e40fed1e
Max Heinritz <maxheinritz@gmail.com>
2017-01-27 10:41:13 -0800

    connect to prod service

--- settings.js
+++ settings.js
@@ -1,7 +1,6 @@
 const settings = {};
 
 settings.isDev = !(process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'staging');
-settings.isHeroku = Boolean(process.env.PORT);
 
 if (settings.isDev) {
   settings.port = 3001;
@@ -9,17 +8,9 @@ if (settings.isDev) {
   settings.clientServerUrl = 'http://localhost:3000';  // React's dev server uses different port.
   settings.scoutServiceUrl = 'http://localhost:5000';
 } else {
-  if (settings.isHeroku) {
-    // Actual prod.
-    settings.port = process.env.PORT;
-    settings.serverUrl = 'https://' + process.env.HOSTNAME;
-    settings.scoutServiceUrl = 'https://scout-service.herokuapp.com';
-  } else {
-    // Compiled prod run locally.
-    settings.port = 3001;
-    settings.serverUrl = 'http://localhost:3001';
-    settings.scoutServiceUrl = 'http://localhost:5000';
-  }
+  settings.port = process.env.PORT;
+  settings.serverUrl = 'https://' + process.env.HOSTNAME;
+  settings.scoutServiceUrl = 'https://scout-service.herokuapp.com';
   settings.clientServerUrl = settings.serverUrl;
 }
 
